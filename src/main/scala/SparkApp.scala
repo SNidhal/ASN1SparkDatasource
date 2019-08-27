5c1fa650ae0f2fc5bfb03a554055524b3a97bf9b
SNIDHAL <nidhal.saadaoui1@esprit.tn>
2019-08-27 14:25:15 +0100

    valid SparkApp

--- src/main/scala/SparkApp.scala
+++ src/main/scala/SparkApp.scala
@@ -16,7 +16,7 @@ object SparkApp {
 
     val as1DataFrame = spark.read.format("asn1V1")
       .option("schemaFileType","asn")
-      .option("mainTag","set")
+      .option("mainTag","sequence")
       .option("schemaFilePath", "src/test/resources/simpleTypes.asn")
       .option("customDecoder","util.CustomJavaDecoder")
       .option("customDecoderLanguage","java")
